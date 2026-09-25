7bf392370a9cc6342fc9959fcf2c1d30da11c6c2
furkan <furkangurbuz5@gmail.com>
2026-09-25 22:02:37 +0200

    Use modern angular in the courses card list

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,53 +1,37 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, input, output} from '@angular/core';
 import {Course} from '../model/course';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import {filter, tap} from 'rxjs/operators';
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false
 })
-export class CoursesCardListComponent implements OnInit {
+export class CoursesCardListComponent {
+  readonly courses = input.required<Course[]>();
+  private readonly dialog = inject(MatDialog);
+  private coursesChanged = output<void>();
 
-  @Input()
-  courses: Course[] = [];
+  editCourse(course: Course) {
+    const dialogConfig = new MatDialogConfig();
 
-  @Output()
-  private coursesChanged = new EventEmitter();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
 
-  constructor(private dialog: MatDialog) {
+    dialogConfig.data = course;
 
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed()
+      .pipe(
+        filter(val => !!val),
+        tap(() => this.coursesChanged.emit())
+      )
+      .subscribe();
   }
-
-  ngOnInit() {
-
-  }
-
-    editCourse(course: Course) {
-
-        const dialogConfig = new MatDialogConfig();
-
-        dialogConfig.disableClose = true;
-        dialogConfig.autoFocus = true;
-        dialogConfig.width = "400px";
-
-        dialogConfig.data = course;
-
-        const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
-        dialogRef.afterClosed()
-            .pipe(
-                filter(val => !!val),
-                tap(() => this.coursesChanged.emit())
-
-            )
-            .subscribe();
-
-
-    }
-
 }
