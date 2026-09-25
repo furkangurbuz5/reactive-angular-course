10eb33ba09a030aae3e06f6eaef7dfe0396e8b04
furkan <furkangurbuz5@gmail.com>
2026-09-25 20:23:56 +0200

    Refactor home component to modern angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,37 +1,19 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {Course} from '../model/course';
 import {Observable} from 'rxjs';
 import {CoursesStore} from '../services/courses.store';
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  private readonly coursesStore = inject(CoursesStore);
 
-  beginnerCourses$: Observable<Course[]>;
-
-  advancedCourses$: Observable<Course[]>;
-
-  constructor(private coursesStore: CoursesStore) {
-
-  }
-
-  ngOnInit() {
-      this.reloadCourses();
-  }
-
-  reloadCourses() {
-
-      this.beginnerCourses$ = this.coursesStore.filterByCategory("BEGINNER");
-
-      this.advancedCourses$ = this.coursesStore.filterByCategory("ADVANCED");
-  }
-
+  protected beginnerCourses$: Observable<Course[]> = this.coursesStore.filterByCategory("BEGINNER");
+  protected advancedCourses$: Observable<Course[]> = this.coursesStore.filterByCategory("ADVANCED");
 }
-
-
