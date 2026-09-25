f7dc8e0b01c2946aabaaf9205f56a194735b927c
furkan <furkangurbuz5@gmail.com>
2026-09-25 20:40:12 +0200

    Small improvements in the saveCourse method

--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -15,31 +15,22 @@ export class CoursesStore {
   private readonly loading = inject(LoadingService);
   private readonly messages = inject(MessagesService);
 
-  private subject = new BehaviorSubject<Course[]>([]);
+  private readonly subject = new BehaviorSubject<Course[]>([]);
   courses$: Observable<Course[]> = this.subject.asObservable();
 
   constructor() {
     this.loadAllCourses();
   }
 
-  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
-
-    const courses = this.subject.getValue();
-
-    const index = courses.findIndex(course => course.id == courseId);
-
-    const newCourse: Course = {
-      ...courses[index],
-      ...changes
-    };
-
-    const newCourses: Course[] = courses.slice(0);
-
-    newCourses[index] = newCourse;
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<void> {
+    const newCourses: Course[] = this.subject.getValue()
+      .map((course: Course): Course => {
+        return (course.id === courseId) ? { ...course, ...changes } : course;
+      });
 
     this.subject.next(newCourses);
 
-    return this.http.put(`/api/courses/${courseId}`, changes)
+    return this.http.put<void>(`/api/courses/${courseId}`, changes)
       .pipe(
         catchError(err => {
           const message = "Could not save course";
